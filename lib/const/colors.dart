14ba19c325dfe931ea5dba70112bf6d82822ba15
Kola <kolya.madridano@gmail.com>
2022-04-02 18:15:43 +0800

    #040222 Add const folder

---
+++ lib/const/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const kPrimary = Color(0xFF1C658C);
+const kSeconday = Color(0xFF398AB9);
+const kLight = Color(0xFFEEEEEE);
---
+++ lib/const/url.dart
@@ -0,0 +1,1 @@
+const baseUrl = "https://api-wrss-cdo.herokuapp.com/api";
