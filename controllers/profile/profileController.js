dcb389efe30607a1a039495ee96a62fbe4a10f1a
Kola <58349866+koolamadridano@users.noreply.github.com>
2022-04-03 00:51:49 +0800

    Update profileController.js

--- controllers/profile/profileController.js
+++ controllers/profile/profileController.js
@@ -57,7 +57,7 @@ const getAllProfiles = (req, res) => {
 
                 const distanceBetween =  distance(d1StartDistance, d1EndDistance, d2StartDistance, d2EndDistance, "K").toFixed(1);
                 console.log(distanceBetween);
-                return res.status(200).json(value);
+                return res.status(200).json(distanceBetween);
 
             })
             .catch((err) => res.status(400).json(err));
