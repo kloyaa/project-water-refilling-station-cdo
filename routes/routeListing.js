e25bcfe9596f149243726aff0e0b90f270c8ba60
Kola <58349866+koolamadridano@users.noreply.github.com>
2022-04-03 10:03:15 +0800

    Update routeListing.js

--- routes/routeListing.js
+++ routes/routeListing.js
@@ -5,19 +5,19 @@ const listing = require("../controllers/listing/listingController");
 
 // @path: api/laundry/listing
 // @Description: Create listing
-router.post("/laundry/listing", (req, res) =>
+router.post("/station/listing", (req, res) =>
     listing.createListing(req, res)
 );
 
 // @path: api/laundry/listing/623fea2e9fe8e253fd772601
 // @Description: Get listings
-router.get("/laundry/listing", (req, res) =>
+router.get("/station/listing", (req, res) =>
     listing.getListings(req, res)
 );
 
 // @path: api/laundry/listing/623fea2e9fe8e253fd772601
 // @Description: Delete listing
-router.delete("/laundry/listing/:id", (req, res) =>
+router.delete("/station/listing/:id", (req, res) =>
     listing.deleteListing(req, res)
 );
 
