2a41591d8c24c391981ec767f91ebb7c71c67a42
Kola <58349866+koolamadridano@users.noreply.github.com>
2022-04-04 14:27:31 +0800

    Update orderController.js

--- controllers/order/orderController.js
+++ controllers/order/orderController.js
@@ -41,14 +41,17 @@ const getOrders = (req, res) => {
                     .then((value) => res.status(200).json(value))
                     .catch((err) => res.status(400).json(err));
             }
-           return Order.find(
-                    { "header.customer.accountId": accountId,
-                      status: { "$ne": 'delivered' }
-                    })
-                    .sort({ "date.createdAt": "desc" }) // filter by date
-                    .select({ __v: 0 }) // Do not return _id and __v
-                    .then((value) => res.status(200).json(value))
-                    .catch((err) => res.status(400).json(err)); 
+          if(accountType === "customer" && status == "not-delivered") {
+               return Order.find(
+                        { "header.customer.accountId": accountId,
+                          status: { "$ne": 'delivered' }
+                        })
+                        .sort({ "date.createdAt": "desc" }) // filter by date
+                        .select({ __v: 0 }) // Do not return _id and __v
+                        .then((value) => res.status(200).json(value))
+                        .catch((err) => res.status(400).json(err)); 
+            }
+
     } catch (error) {
         console.error(error);
     }
